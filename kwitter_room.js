dab443c66a275e6a763d38b549f2bae8feaaeb1e
VV <85766798+Vedant-cll@users.noreply.github.com>
2021-09-24 14:24:57 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+function logout(){
+    window.location="index.html";
+}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAgX9I1dfTA0JuAF2EocPjAjGufLg91-SU",
+    authDomain: "kwitter-project-692c0.firebaseapp.com",
+    databaseURL: "https://kwitter-project-692c0-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-692c0",
+    storageBucket: "kwitter-project-692c0.appspot.com",
+    messagingSenderId: "226743716561",
+    appId: "1:226743716561:web:de8b1f8a464a5bdb3a53f0"
+  };
+  
+  // Initialize Firebase
